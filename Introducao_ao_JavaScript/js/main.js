9196f89782cc2139c26e5b56087ecdb381e472a4
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2022-09-30 13:36:48 -0300

    Testando tipagem

--- Introducao_ao_JavaScript/js/main.js
+++ Introducao_ao_JavaScript/js/main.js
@@ -1,2 +1,10 @@
-alert("Meu primeiro Js");
+var nome = "Andrei Ferrari";
+var idade = 20;
+var idade2= 10
+frase = "BRASIL MY TEAM"
+//alert(nome+" tem " + idade + " anos");
+//alert(idade+idade2);
 
+console.log(nome);
+console.log(idade+idade2)
+console.log(frase)
