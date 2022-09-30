ee5f43db988c1dca37af3ca7ea4c3cb5346072a2
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2022-09-30 15:24:43 -0300

    Validade Idade

--- 2-1.Desenvolva_paginas_web_com_JavaScript/js/main.js
+++ 2-1.Desenvolva_paginas_web_com_JavaScript/js/main.js
@@ -5,7 +5,19 @@ function setReplace(frase, nome,novo_nome){
     return frase.replace(nome,novo_nome)
 }
 
-
 alert(soma(5,10))
 frase = "Vai brasil"
 alert(setReplace(frase,"brasil","japao"))
+
+function validaIdade(i){
+    if(i >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar
+}
+
+let idade = prompt("Informe uma idade: ")
+
+console.log(validaIdade(idade))
