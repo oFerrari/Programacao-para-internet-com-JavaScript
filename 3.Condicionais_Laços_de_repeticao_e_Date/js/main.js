c7557cf5305ed3250019b097ee41518cd4facee5
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2022-09-30 14:59:10 -0300

    Estruturas de Repetição

---
+++ 3.Condicionais_Laços_de_repeticao_e_Date/js/main.js
@@ -0,0 +1,24 @@
+let idade = prompt("qual sua idade");
+
+if(idade >= 18){
+    alert("maior de idade");
+}
+
+let count = 0;
+while(count < 5){
+    console.log(count);
+    count++;
+}
+console.log("\n\n") 
+
+let num;
+
+for(num = 0;num <= 5;num++){
+    console.log(num);   
+}
+
+let data = new Date();
+console.log(data)
+console.log(data.getDay())
+console.log(data.getHours())
+console.log(data.getMinutes())
