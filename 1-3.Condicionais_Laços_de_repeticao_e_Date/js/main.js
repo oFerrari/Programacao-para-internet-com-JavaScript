807e39ff002baf8c0fe78ef12a2e460cebf1d9b3
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2023-01-03 09:41:36 -0300

    ✨ Finalizando aula

--- 1-3.Condicionais_Laços_de_repeticao_e_Date/js/main.js
+++ 1-3.Condicionais_Laços_de_repeticao_e_Date/js/main.js
@@ -2,13 +2,17 @@ let idade = prompt("qual sua idade");
 
 if(idade >= 18){
     alert("maior de idade");
+}else{
+    alert("menor de idade")
 }
 
 let count = 0;
+
 while(count < 5){
     console.log(count);
     count++;
 }
+
 console.log("\n\n") 
 
 let num;
@@ -16,6 +20,8 @@ let num;
 for(num = 0;num <= 5;num++){
     console.log(num);   
 }
+
+console.log("\n\n")
 
 let data = new Date();
 console.log(data)
